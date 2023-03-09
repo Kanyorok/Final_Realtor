725bc7b05cec3007651d1c75f17fb4bc88284da2
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-09 11:30:15 +0300

    Created pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Offers from "./Pages/Offers";
+import Profile from "./Pages/Profile";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Forgotpassword from "./Pages/Forgotpassword";
 
 function App() {
   return (
     <>
-      <h1 className='text-2xl bg-red-500'>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path= "/" element={<Home/>} />
+          <Route path= "/offers" element={<Offers/>} />
+          <Route path= "/profile" element={<Profile/>} />
+          <Route path= "/sign-in" element={<Signin/>} />
+          <Route path= "/sign-up" element={<Signup/>} />
+          <Route path= "/forgot-password" element={<Forgotpassword/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Pages/Forgotpassword.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Forgotpassword() {
+  return (
+    <div>Forgot Password</div>
+  )
+}
